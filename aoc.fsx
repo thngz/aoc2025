a4f706814736c16482480bab5e61f2b65ede2d4e
gkiviv <georgkivivali7@gmail.com>
2025-12-01 23:28:45 +0200

    pt 2 and cleanups

--- aoc.fsx
+++ aoc.fsx
@@ -29,30 +29,32 @@ module Day1 =
                       turns = getNumFromStr ins }
                 | _ -> failwith "Bruh what")
 
-        let rotateLeft (n: int) (by: int) : int = snd (System.Math.DivRem(n - by, 100))
-
-        let rotateRight (n: int) (by: int) : int = snd (System.Math.DivRem(n + by, 100))
+        
+        let applyRotation (n: int) (by: int) =
+            let rem = snd (System.Math.DivRem(n + by, 100))
+            
+            if rem < 0 then rem + 100 else rem
 
         snd (
             List.fold
                 (fun acc ins ->
                     match ins.direction with
                     | L ->
-                        let rotation = rotateLeft (fst acc) ins.turns
+                        let rotation = applyRotation (fst acc) -ins.turns
                         (rotation, if rotation = 0 then snd acc + 1 else snd acc)
                     | R ->
-                        let rotation = rotateRight (fst acc) ins.turns
+                        let rotation = applyRotation (fst acc) ins.turns
                         (rotation, if rotation = 0 then snd acc + 1 else snd acc))
                 (50, 0)
                 instructions
         )
 
-    // ----------------------------------------------------------------------------------------------
+    // -----------------------------------------PART 2-----------------------------------------------------
 
     let getPart2: int =
-        // let input = System.IO.File.ReadAllLines("./day1/input.txt") |> Array.toList
-        let input =
-            [ "L68"; "L30"; "R48"; "L5"; "R60"; "L55"; "L1"; "L99"; "R14"; "L82"; "L201" ]
+        let input = System.IO.File.ReadAllLines("./day1/input.txt") |> Array.toList
+        // let input =
+        //     [ "L68"; "L30"; "R48"; "L5"; "R60"; "L55"; "L1"; "L99"; "R14"; "L82"; ]
 
         let instructions: List<Instruction> =
             input
@@ -66,35 +68,32 @@ module Day1 =
                       turns = getNumFromStr ins }
                 | _ -> failwith "Bruh what")
 
-        let rotateLeft (n: int) (by: int) : int = snd (System.Math.DivRem(n - by, 100))
-        // 100 - System.Math.Abs(rem)
-        // snd (System.Math.DivRem(n - by, 100))
+        let applyRotationAndGetCrossings (n: int) (by: int): (int * int) =
+            let rem = (n + by) % 100
+            let nextPos = if rem < 0 then rem + 100 else rem
 
-        let rotateRight (n: int) (by: int) : int =
-            // let res = (n + by)
-            // if res >= 100 then res - 100 else res
-            snd (System.Math.DivRem(n + by, 100))
+            let crossings =
+                if by > 0 then 
+                    let dist = if n = 0 then 100 else 100 - n
+                    if by >= dist then 1 + (by - dist) / 100 else 0
+                else
+                    let dist = if n = 0 then 100 else n
+                    let absBy = -by
+                    if absBy >= dist then 1 + (absBy - dist) / 100 else 0
+            (nextPos, crossings)
 
-        // let rem = snd (System.Math.DivRem(n + by, 100))
-        // 100 - System.Math.Abs(rem)
-
-
+        // 6153 too high
         snd (
             List.fold
                 (fun acc ins ->
-                    printf "%A\n" acc
                     match ins.direction with
                     | L ->
-                        let rotation = rotateLeft (fst acc) ins.turns
-                        let rotate = 100 - System.Math.Abs(rotation)
-                        // printf "Rotated left %A\n" rotation
-                        (rotation, (if rotation = 0 then snd acc + 1 else snd acc))
+                        let rotation, crossings = applyRotationAndGetCrossings (fst acc) -ins.turns
+                        (rotation,  snd acc + crossings)
                     | R ->
-                        let rotation = rotateRight (fst acc) ins.turns
-                        let rotate = 100 - System.Math.Abs(rotation)
-                        // printf "Rotated right %A\n" rotation
+                        let rotation, crossings = applyRotationAndGetCrossings (fst acc) ins.turns
 
-                        (rotation, (if rotation = 0 then snd acc + 1 else snd acc)))
+                        (rotation,  snd acc + crossings))
                 (50, 0)
                 instructions
         )
