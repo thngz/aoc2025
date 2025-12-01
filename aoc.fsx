f89b295ca4cd4bcf92bac5ffd4f7647422430d95
gkiviv <georgkivivali7@gmail.com>
2025-12-01 19:42:25 +0200

    part 1

--- aoc.fsx
+++ aoc.fsx
@@ -1,4 +1,5 @@
 open System.Text.RegularExpressions
+// open System.Math
 
 module Day1 =
     type Direction =
@@ -14,7 +15,8 @@ module Day1 =
 
     let getPart1: int =
         let input = System.IO.File.ReadAllLines("./day1/input.txt") |> Array.toList
-        // let input = [ "L68"; "L30"; "R48"; "L5"; "R60"; "L55"; "L1"; "L99"; "R14"; "L82" ]
+        // printf "%A" input
+        // let input = [ "L68"; "L30"; "R48"; "L5"; "R60"; "L55"; "L1"; "L99"; "R14"; "L82"; "L201" ]
 
         let instructions: List<Instruction> =
             input
@@ -28,13 +30,9 @@ module Day1 =
                       turns = getNumFromStr ins }
                 | _ -> failwith "Bruh what")
 
-        let rotateLeft (n: int) (by: int) : int =
-            let res = (n - by)
-            if res < 0 then res + 100 else res
+        let rotateLeft (n: int) (by: int) : int = snd (System.Math.DivRem(n - by, 100))
 
-        let rotateRight (n: int) (by: int) : int =
-            let res = (n + by)
-            if res >= 100 then res - 100 else res
+        let rotateRight (n: int) (by: int) : int = snd (System.Math.DivRem(n + by, 100))
 
         snd (
             List.fold
@@ -42,10 +40,65 @@ module Day1 =
                     match ins.direction with
                     | L ->
                         let rotation = rotateLeft (fst acc) ins.turns
+                        // printf "Rotated left %A\n" rotation;
                         (rotation, if rotation = 0 then snd acc + 1 else snd acc)
                     | R ->
                         let rotation = rotateRight (fst acc) ins.turns
+                        // printf "Rotated right %A\n" rotation;
                         (rotation, if rotation = 0 then snd acc + 1 else snd acc))
+                (50, 0)
+                instructions
+        )
+
+    // ----------------------------------------------------------------------------------------------
+
+    let getPart2: int =
+        // let input = System.IO.File.ReadAllLines("./day1/input.txt") |> Array.toList
+        // printf "%A" input
+        let input =
+            [ "L68"; "L30"; "R48"; "L5"; "R60"; "L55"; "L1"; "L99"; "R14"; "L82"; "L201" ]
+
+        let instructions: List<Instruction> =
+            input
+            |> List.map (fun x ->
+                match x with
+                | ins when ins.StartsWith("L") ->
+                    { direction = L
+                      turns = getNumFromStr ins }
+                | ins when ins.StartsWith("R") ->
+                    { direction = R
+                      turns = getNumFromStr ins }
+                | _ -> failwith "Bruh what")
+
+        let rotateLeft (n: int) (by: int) : int = snd (System.Math.DivRem(n - by, 100))
+        // 100 - System.Math.Abs(rem)
+        // snd (System.Math.DivRem(n - by, 100))
+
+        let rotateRight (n: int) (by: int) : int =
+            // let res = (n + by)
+            // if res >= 100 then res - 100 else res
+            snd (System.Math.DivRem(n + by, 100))
+
+        // let rem = snd (System.Math.DivRem(n + by, 100))
+        // 100 - System.Math.Abs(rem)
+
+
+        snd (
+            List.fold
+                (fun acc ins ->
+                    match ins.direction with
+                    | L ->
+                        let rotation = rotateLeft (fst acc) ins.turns
+                        let rotate = 100 - System.Math.Abs(rotation)
+                        // printf "Rotated left %A\n" rotation
+                        printf "Rotated left %A\n" rotate
+                        (rotate, (if rotation = 0 then snd acc + 1 else snd acc))
+                    | R ->
+                        let rotation = rotateRight (fst acc) ins.turns
+                        let rotate = 100 - System.Math.Abs(rotation)
+                        printf "Rotated right %A\n" rotate
+
+                        (rotate, (if rotation = 0 then snd acc + 1 else snd acc)))
                 (50, 0)
                 instructions
         )
