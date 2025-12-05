4122e1669215aa6ce460e1ba18befab2fb323e12
gkiviv <georgkivivali7@gmail.com>
2025-12-05 23:18:57 +0200

    beginning day2

--- aoc.fsx
+++ aoc.fsx
@@ -101,18 +101,39 @@ module Day1 =
                 instructions
         )
 
+module Day2 =
+    type Range = { start: int; ending: int }
+
+    let getDay1: int =
+        let input =
+            "11-22,95-115,998-1012,1188511880-1188511890,222220-222224, 1698522-1698528,446443-446449,38593856-38593862,565653-565659, 824824821-824824827,2121212118-2121212124"
+
+        let ranges: Range list =
+            input.Split ","
+            |> Array.map (fun range ->
+                let parts = range.Split '-'
+                { start = int parts[0]; ending = int parts[1] })
+            |> Array.toList
+
+        
+        0
+
+
 module Day3 =
     let getDay1: int =
         // let input =
         //     [ "987654321111111"; "811111111111119"; "234234234234278"; "818181911112111" ]
-        
+
         let input = System.IO.File.ReadAllLines("./day3/input.txt") |> Array.toList
 
         let getLargestJoltage (pack: string) : int =
             let digits = pack |> Seq.map (fun c -> int c - int '0') |> Seq.toArray
+
             seq {
-                for i in 0.. digits.Length - 2 do
+                for i in 0 .. digits.Length - 2 do
                     for j in i + 1 .. digits.Length - 1 do
                         yield digits[i] * 10 + digits[j]
-            } |> Seq.max
+            }
+            |> Seq.max
+
         List.fold (fun acc pack -> acc + getLargestJoltage pack) 0 input
